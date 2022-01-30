5fa54ea5471d6215ddb5ffaa6e19f0683596916d
Tushan9005 <dastushan9005@gmail.com>
2022-01-30 20:30:20 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const app= express();
+
+const port = process.env.PORT || 3000;
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Middleware
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.post('/contact',urlencodedParser, function(req,res){
+    console.log(req.body);
+    res.render('success',{data: req.body});
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
